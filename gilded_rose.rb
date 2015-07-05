65b976d10dc4cfe4a13b7344a1d122240fef3226
eduardopoleo <eduardopoleo@gmail.com>
2015-07-05 12:37:32 -0400

    My first solution

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,63 +1,63 @@
 def update_quality(items)
   items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
-    end
     if item.name != 'Sulfuras, Hand of Ragnaros'
       item.sell_in -= 1
     end
+
+    case item.name
+    when 'NORMAL ITEM'
+      update_nomal_item_quality(item)
+    when 'Aged Brie'
+      update_aged_brie_quality(item)
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      update_backtage_passess_quality(item)
+    when 'Conjured Mana Cake'
+      update_conjured_item_quality(item)
+    else
+      item.quality
+    end
+  end
+end
+
+
+def update_nomal_item_quality(item)
+  if item.quality > 0
+    item.sell_in < 0 ? item.quality -= 2 : item.quality -= 1
+  end
+end
+
+def update_aged_brie_quality(item)
+  if item.quality < 50
+    item.quality += 1
+    if item.sell_in < 0 && item.quality < 50
+      item.quality += 1
+    end
+  end
+end
+
+def update_backtage_passess_quality(item)
+  if item.quality < 50
     if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-        end
+      item.quality = 0
+    else
+      item.quality += 1
+
+      if item.sell_in < 10 && item.quality < 50
+        item.quality += 1
+      end
+
+      if item.sell_in < 5 && item.quality < 50
+        item.quality += 1
       end
     end
   end
 end
 
-# DO NOT CHANGE THINGS BELOW -----------------------------------------
+def update_conjured_item_quality(item)
+  if item.quality > 0
+    item.sell_in <= 0 ? item.quality -= 4 : item.quality -= 2
+  end
+end
+
 
 Item = Struct.new(:name, :sell_in, :quality)
-
-# We use the setup in the spec rather than the following for testing.
-#
-# Items = [
-#   Item.new("+5 Dexterity Vest", 10, 20),
-#   Item.new("Aged Brie", 2, 0),
-#   Item.new("Elixir of the Mongoose", 5, 7),
-#   Item.new("Sulfuras, Hand of Ragnaros", 0, 80),
-#   Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-#   Item.new("Conjured Mana Cake", 3, 6),
-# ]
-
